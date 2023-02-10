ebcd998e68109600c59407107bdb665a3f594e6f
DESKTOP-AVA158Q\ian_l <mano96@live.com.mx>
2023-02-10 16:11:07 -0600

    cambios con login

--- nodos/views.py
+++ nodos/views.py
@@ -1,25 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 # Create your views here.
+
+
 def home(request):
-    return render (request, 'home.html', {
-      'form':UserCreationForm
-  })
+    return render(request, 'home.html', {
+        'form': UserCreationForm
+    })
+
 
 def signup(request):
-    if request.method=='GET':
-       return render (request, 'signup.html', {
-      'form':UserCreationForm
-  })
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
     else:
-        if request.POST['password1']==request.POST['password2']:
-            #register user
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
             try:
-                user =User.objets.create_user(username=request.POST['username'], password=request.POST['password'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created sucessfully')
-            except:
-                return HttpResponse('username already exists')
-        return HttpResponse('passwords do not match')
+                login(request, user)
+                return redirect('lp')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Passwords do not match'
+        })
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def lp(request):
+    return render(request, 'lp.html')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user=authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+            'form': AuthenticationForm,
+            'error':'Username or password is incorrect'
+             })
+        else:
+            login(request, user)     
+        return redirect('lp')
+
--- redvitab/urls.py
+++ redvitab/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+    path('landingpage/', views.lp, name='lp'),
     
 ]
